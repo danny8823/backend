fae25fcec387a654b8c51ad17819b4fbf27acaab
danny8823 <dannyyoo714@gmail.com>
2024-10-07 19:17:08 -0700

    Added mongodb connection, express.json() and port

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 1000;
+const server = express();
+const URL = 'mongodb+srv://dannyyoo714:Jesuschrist8823!@todo-db.8x2pz.mongodb.net/todo?retryWrites=true&w=majority&appName=todo-db'
+
+mongoose
+    .connect(URL)
+    .then(()=>{
+        console.log('DB connected..')
+    })
+    .catch((e)=>{
+        console.log(e)
+    })
+
+server.use(express.json())
+
+server.listen(PORT,() => {
+    console.log(`Server listening on port: ${PORT}`)
+})
